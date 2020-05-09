ff9be3f03c58f41feee9154d8ac72118f01ed182
Shubham Deshpande <shubhamdesh11153@gmail.com>
2020-05-09 11:57:41 -0700

    Initial Version

---
+++ api/scripts/generate_data.mongo.js
@@ -0,0 +1,24 @@
+/* eslint-disable linebreak-style */
+/* global db print */
+/* eslint no-restricted-globals: "off" */
+
+const names = ['USPA', 'Adidas', 'Reebok', 'Holister', 'Nike', 'Perfume'];
+const categories = ['shirt', 'jeans', 'sweater', 'jacket', 'accessories'];
+
+const initialCount = db.issues.count();
+
+for (let i = 0; i < 100; i += 1) {
+  const Name = names[Math.floor(Math.random() * 6)];
+  const Category = categories[Math.floor(Math.random() * 5)];
+  const Price = Math.ceil(Math.random() * 20);
+  const Image = `url${i}`;
+  const id = initialCount + i + 1;
+  const prd = {
+    id, Name, Price, Image, Category,
+  };
+  db.items.insertOne(prd);
+}
+
+const count = db.items.count();
+db.counters.update({ _id: 'items' }, { $set: { current: count } });
+print('New product count:', count);
